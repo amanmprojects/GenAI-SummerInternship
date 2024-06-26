75fc01656eeeec2145b0c9bc33f789ffa84b702b
Aman M <amanmstudies@gmail.com>
2024-06-26 16:03:43 +0530

    Added FastAPI Server and groq functionality

--- fastapi_server.py
+++ fastapi_server.py
@@ -1,45 +1,84 @@
-from fastapi import FastAPI
-# from utils import getResults
+from fastapi import FastAPI, UploadFile, File, Response, Query      
+from fastapi.responses import FileResponse, StreamingResponse, JSONResponse
+from io import BytesIO   
+import os
 import time
 from services import WeaviateQueryService
+from pydantic import BaseModel
+from typing import List
+# from json import JsonRes
 print("All imports successfull")
 
 
 
 query_service = WeaviateQueryService("Products")
 
-while True:
-    query_service.get_results(input("Enter a query: "), 10, True)
-    print("\n")
-    
-
-
-# # init weaviate client
-# client = weaviate.connect_to_local()
-# products = client.collections.get("Products")
-
-
-
-# print("All initializations complete")
-
-# start = time.time()
-# results = getResults("recommend me some sexy adidas tops girls", 10, model, products)
-# end = time.time()
-# # print(results)
-# for result in results:
-#     print(result.properties['productDisplayName'])
-
-# print(f"time taken : {end-start} seconds")
-
-
-
 
 
 
 app = FastAPI()
 
+# print("All initializations complete")
+
+
+class ProductResponse(BaseModel):
+    id: int
+    displayNames: str
+    masterCategories: str
+    subCategories: str
+    articleTypes: str
+    baseColours: str
+    seasons: str
+    years: int
+    usages: str
+    descriptions: str
+    averageRatings: float
+    numberOfRatings: int
+    # Prices: str
+    imagePath: str
+    class Config:
+        orm_mode = True
+
+
+
+
+@app.post("/query", response_model=List[ProductResponse])
+async def get_product_details(query: str = "General Products", top_n: int = 10, groq_simplify: bool = True):
+    products = query_service.get_results(query=query, top_n=top_n, groq_llama_simplfy=groq_simplify,print_responses_name=True)
+    response = []
+
+    for product in products:
+        response.append(ProductResponse(
+            id = product.properties ['product_id'],
+            displayNames=product.properties['productDisplayName'],
+            masterCategories=product.properties['masterCategory'],
+            subCategories=product.properties['subCategory'],
+            articleTypes=product.properties['articleType'],
+            baseColours=product.properties['baseColour'],
+            seasons=product.properties['season'],
+            years=product.properties['year'],
+            usages=product.properties['usage'],
+            descriptions=product.properties['description'],
+            averageRatings=product.properties['averageRating'],
+            numberOfRatings=product.properties['numberOfRatings'],
+            # Prices=product.properties['Price'],
+            imagePath=product.properties['imagePath']
+        ))
+    return response
+
+@app.get("/images/{image_name}")
+async def get_image(image_name: str):
+    file_path = f"images/{image_name}"
+    return FileResponse(file_path)
+
+
+
+
 @app.get('/')
 def getting():
     return "Hello world"
+
+
+
 
 
--- services.py
+++ services.py
@@ -3,15 +3,20 @@ from sentence_transformers import SentenceTransformer
 import weaviate
 import weaviate.classes as wvc
 import warnings
+from dotenv import load_dotenv
+from groq import Groq
+import os
+from prompt_template import prompt1
+
 
 # Suppressing warning
 warnings.filterwarnings("ignore", category=FutureWarning, message=".*resume_download.*")
 
-
+load_dotenv()
 
 # Setting the model
 MODEL_LIST = ["sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2", "paraphrase-MiniLM-L6-v2", "all-MiniLM-L6-v2", "paraphrase-MiniLM-L12-v2"]
-MODEL_NAME = MODEL_LIST[1]
+MODEL_NAME = 'all-MiniLM-L12-v2'
 
 model = SentenceTransformer(MODEL_NAME)
 
@@ -19,17 +24,27 @@ model = SentenceTransformer(MODEL_NAME)
 # init weaviate client
 client = weaviate.connect_to_local()
 
-print("Hello world")
+
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 
 
 class WeaviateQueryService:
     def __init__(self, collection:str):
         self.model = model
         self.collection = client.collections.get(collection)
+        self.groqHandler = groqHandler(api_key=GROQ_API_KEY)
 
-    def get_results(self, query: str, top_n: int, print_responses_name:bool = False) -> List[Dict[str, Any]]:
+    def get_results(self, query: str, top_n: int,groq_llama_simplfy :  bool = True ,print_responses_name:bool = False) -> List[Dict[str, Any]]:
+        
+        if groq_llama_simplfy:
+            query_mod = self.groqHandler.query_transform(query=query)
+        else: query_mod = query
+    
+
+
+
         # Vectorizing the query
-        query_vector = self.model.encode(query, convert_to_tensor=True).tolist()
+        query_vector = self.model.encode(query_mod, convert_to_tensor=True).tolist()
 
         # Retrieving the Top n results
         responses = self.collection.query.near_vector(
@@ -40,10 +55,38 @@ class WeaviateQueryService:
 
         if print_responses_name:
             for response in responses:
-                print(response.properties)
+                print(response.properties['productDisplayName'])
 
         
         return responses
+    
+class groqHandler:
+    def __init__(self, api_key) -> None:
+        self.api_key = api_key
+        self.init()
+
+    def init(self):
+        self.groq_client = Groq(
+            api_key=self.api_key
+        )
+    
+    def query_transform(self, query:str):
+            response = self.groq_client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "system",
+                        'content': prompt1
+                    },
+                    {
+                        "role": "user",
+                        "content": query,
+                    },
+                ],
+                model="llama3-70b-8192",
+                )
+            print("YElo", response.choices[0].message.content)
+            return str(response.choices[0].message.content)
+        
 
 
 
